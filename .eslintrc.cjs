5319d0a9b102f050051682a25803309a2581649e
vaibhav <vsaxena@enphaseenergy.com>
2025-03-15 03:16:37 +0530

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:storybook/recommended'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
---
+++ assets/Button.stories-DiE6ZJyb.js
@@ -0,0 +1,5 @@
+import{j as u}from"./jsx-runtime-D_zvdyIk.js";const a=({children:s,variant:n="primary",className:m,...p})=>{const i="px-4 py-2 rounded font-medium",d=n==="primary"?"bg-blue-600 text-white":"bg-gray-100 text-black border";return u.jsx("button",{className:`${i} ${d} ${m}`,...p,children:s})};a.__docgenInfo={description:"",methods:[],displayName:"Button",props:{variant:{required:!1,tsType:{name:"union",raw:"'primary' | 'secondary'",elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"}]},description:"",defaultValue:{value:"'primary'",computed:!1}},text:{required:!1,tsType:{name:"string"},description:"Example prop - describe it properly"}},composes:["ButtonHTMLAttributes"]};const l={title:"Components/Button",component:a,tags:["autodocs"]},e={args:{text:"Button rendered from Storybook"}};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    text: 'Button rendered from Storybook'
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const y=["Primary"];export{e as Primary,y as __namedExportsOrder,l as default};
