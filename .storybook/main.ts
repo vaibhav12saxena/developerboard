f4c1224f4d861550cfb86d38f4dd6a9dc299e979
vaibhav <vsaxena@enphaseenergy.com>
2025-03-16 03:05:51 +0530

    fix:fixing main.ts in story book to handle atoms folder

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: [
     '../src/docs/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)',
+    '../src/**/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)',
   ],
   addons: [
     '@storybook/addon-essentials',
